c23c7752a6538512b0b71e11c0424c3f850ac1f8
Marouan <marouan.yaouaddou@student.vinci.be>
2025-10-08 17:48:29 +0200

    new:ex2.1

---
+++ exercices/2.1-2-3-4/src/App.tsx
@@ -0,0 +1,80 @@
+import './App.css'
+
+
+  interface HeaderProps {
+    title:string
+  }
+
+const Header = (props: HeaderProps) => {
+  return (
+    <header>
+      <h1 className="animate_animated animate_bounce">{props.title}</h1>
+    </header>
+  )
+}
+
+  interface MovieProps {
+    title:string,
+    director:string,
+  }
+  
+  interface CinemaProps {
+      name:string,
+      movies:MovieProps[],
+    }
+
+  const Cinema = (props: CinemaProps) => {
+    return (
+      <section>
+        <h2>{props.name}</h2>
+        <ul>
+          {props.movies.map((movie) => (
+            <li>
+              <strong>{movie.title}</strong> - Réalisateur : {movie.director}
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+  }
+
+  
+
+const App = () => {
+  const pageTitle = "Informations sur les films dans les cinémas";
+  const movies1: MovieProps[] = [
+      { 
+        title:"Film 1 - DeBrouckère",
+        director: "Director A",
+      },
+      {
+        title:"Film 2 - DeBrouckère",
+        director:"Director B",
+}];
+  const cinema1 : CinemaProps = {name:"UGC DeBrouckère",movies:movies1};
+  const movies2 : MovieProps[] = [
+    {
+      title:"Film 1 - Toison d'Or",
+      director:"Director C",
+    },
+    {
+      title:"Film 2 - Toison d'Or",
+      director:"Director D",
+    }
+  ];
+  const cinema2 : CinemaProps = {name:"UGC Toison d'Or",movies:movies2};
+
+
+  return (
+    <div>
+      <Header title={pageTitle}/>
+      <Cinema name={cinema1.name} movies={cinema1.movies}/>
+      <Cinema name={cinema2.name} movies={cinema2.movies}/>
+    </div>
+  );
+};
+
+
+
+
+export default App;
